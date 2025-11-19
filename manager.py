d564604afb96aefce164fd19663d9f1b2a61f3db
wtgftw <wojtek.trojnara123@wp.pl>
2025-11-19 11:16:00 +0100

    refactor(manager): changed Manager logics

--- manager.py
+++ manager.py
@@ -2,7 +2,30 @@ from menu import Menu
 
 class Manager:
     def __init__(self) -> None:
-        pass
+        self._menu = Menu()
 
-    def start(self) -> None:
-        Menu.welcome()
+    def run(self) -> None:
+        self._menu.start()
+
+        while True:
+            choice = self._menu.get_user_choice()
+            match choice:
+                case 1:
+                    print("You selected Encrypt with ROT13")
+                case 2:
+                    print("You selected Encrypt with ROT47")
+                case 3:
+                    print("You selected Encrypt with Custom Shift")
+                case 4:
+                    print("You selected Decrypt with ROT13")
+                case 5:
+                    print("You selected Decrypt with ROT47")
+                case 6:
+                    print("You selected Encrypt with Custom Shift")
+                case 0:
+                    print("Exiting the program. Goodbye!")
+                    break
+                case _:
+                    print("Invalid choice. Please try again.")
+      
+    
