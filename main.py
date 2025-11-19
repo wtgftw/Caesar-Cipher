d66c0b476e01aa7aa32ee06cf93910b9765dfbd5
wtgftw <wojtek.trojnara123@wp.pl>
2025-11-19 11:18:52 +0100

    refactor(main): swapped menu with manager

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
-from menu import Menu
 from manager import Manager
 
 class CaesarCipherFacade:
     def __init__(self) -> None:
-        self._menu = Menu()
         self._manager = Manager()
 
     def run(self) -> None:
-        self._menu.start()
+        self._manager.run()
 
 def main():
     app = CaesarCipherFacade()
