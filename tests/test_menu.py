e7394615da28552479beb76805f2a1760577f157
wtgftw <wojtek.trojnara123@wp.pl>
2026-01-29 15:05:27 +0100

    added main menu tests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -7,7 +7,22 @@ def test_start_should_print_welcome_message_and_call_main_menu():
     menu = Menu()
 
     with mock.patch('builtins.print') as mocked_print:
-        menu.start()
+        with mock.patch('src.menu.Menu.main_menu') as mocked_main_menu:
+            menu.start()
 
     assert mocked_print.mock_has_any_call("Welcome in Caesar Cipher encryptor")
+    mocked_main_menu.assert_called_once()
 
+
+def test_main_menu_should_print_11_menu_options():
+    menu = Menu()
+
+    with mock.patch('builtins.print') as mocked_print:
+        menu.main_menu()
+
+    
+    assert mocked_print.mock_has_calls(menu.menu_options)
+
+
+def test_get_user_choice_should_raise_ValueError_when():
+    pass
